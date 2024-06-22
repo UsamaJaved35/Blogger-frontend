fd3e98e291d0d7ff780269a2356ea329de4c463b
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-22 23:07:21 +0500

    added routing in app

--- blogger-frontend/src/App.js
+++ blogger-frontend/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import BlogList from './components/BlogList';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/all-posts" element={<BlogList filter="all-posts" />} />
+          <Route path="/my-posts" element={<BlogList filter="my-posts" />} />
+          <Route exact path="/" element={<h1> Welcome to Blogger</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
