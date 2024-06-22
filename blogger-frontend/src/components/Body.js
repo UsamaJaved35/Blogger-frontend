c6e7a3b38ce8854cbdb902b613f3f38d9ccea2de
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-22 23:08:12 +0500

    components for landing page added

---
+++ blogger-frontend/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './LandingPage'
+
+const Body = () => {
+  return (
+    <div>
+      <LandingPage/>
+    </div>
+  )
+}
+
+export default Body
---
+++ blogger-frontend/src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <h1 style={{fontSize:"80px"}}>
+            Welcome to Blogger!<br/>
+            Please SignIn / SignUp to continue
+        </h1>
+    </div>
+  )
+}
+
+export default LandingPage
