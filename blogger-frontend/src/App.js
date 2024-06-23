6d47b7dbe19d1068c128fafe90bb596441860049
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-23 17:04:35 +0500

    added routes for create,edit and signin signup forms

--- blogger-frontend/src/App.js
+++ blogger-frontend/src/App.js
@@ -6,6 +6,8 @@ import BlogList from './components/BlogList';
 import Body from './components/Body';
 import BlogPostForm from './components/BlogPostForm';
 import PrivateRoute from './components/PrivateRoute';
+import SignInForm from './components/SignInForm';
+import SignUpForm from './components/SignUpForm';
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
@@ -13,8 +15,8 @@ function App() {
   useEffect(() => {
     // Check if the user is signed in by checking the presence of a token in localStorage
     const token = localStorage.getItem('token');
-    // setIsSignedIn(!!token);
-    setIsSignedIn(false)
+    setIsSignedIn(!!token);
+    // setIsSignedIn(false)
   }, []);
   return (
     <Router>
@@ -22,10 +24,12 @@ function App() {
         <Header isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn}  />
         <Routes>
           <Route path="/" element={<Body isSignedIn={isSignedIn} />} />
+          <Route path="/sign-in" element={<SignInForm setIsSignedIn={setIsSignedIn}/>}/>
+          <Route path="/sign-up" element={<SignUpForm/>}/>
           <Route path="/all-posts" element={<PrivateRoute element={BlogList} isSignedIn={isSignedIn} filter="all-posts" />} />
           <Route path="/my-posts" element={<PrivateRoute element={BlogList} isSignedIn={isSignedIn} filter="my-posts" />} />
-          <Route path="/create" element={<PrivateRoute element={BlogPostForm} isSignedIn={isSignedIn} onSuccess={() => window.location.href = '/all-posts'} />} />
-          <Route path="/edit/:id" element={<PrivateRoute element={BlogPostForm} isSignedIn={isSignedIn} onSuccess={() => window.location.href = '/all-posts'} />} />
+          <Route path="/create" element={<PrivateRoute element={BlogPostForm} isSignedIn={isSignedIn} />} />
+          <Route path="/edit/:id" element={<PrivateRoute element={BlogPostForm} isSignedIn={isSignedIn} />} />
         </Routes>
       </div>
     </Router>
