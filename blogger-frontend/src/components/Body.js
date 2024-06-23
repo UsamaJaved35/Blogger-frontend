4c4e19c8ccf67e5aebbb1889de3562a70650bc21
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-06-23 17:06:46 +0500

    removed console.log

--- blogger-frontend/src/components/Body.js
+++ blogger-frontend/src/components/Body.js
@@ -2,7 +2,6 @@ import React from 'react'
 import LandingPage from './LandingPage'
 
 const Body = ({isSignedIn}) => {
-  console.log(isSignedIn);
   return (
     <div>
       { isSignedIn ? <h1>Welcome to Blogger</h1>:<LandingPage/>}
